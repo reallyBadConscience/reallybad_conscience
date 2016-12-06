8ff5657010e9e2d31607028b625af8536144b811
reallyBadConscience <really.bad.conscience@gmail.com>
2016-12-06 11:12:49 +0100

    changed Language to Italian

--- src/reallybad_conscience/MyApp.java
+++ src/reallybad_conscience/MyApp.java
@@ -3,7 +3,7 @@ package reallybad_conscience;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Bonjour le monde!");
+		System.out.println("Ciao Mondo!");
 	}
 
 }
